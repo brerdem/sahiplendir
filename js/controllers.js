fa0cb037432dab4bba0d92c1f14a2cd10a10ce3a
Burak Erdem <falsemech@gmail.com>
2015-01-04 22:11:01 +0200

    controllers.js and home.html changed

--- js/controllers.js
+++ js/controllers.js
@@ -7,7 +7,7 @@ angular.module('Sahiplendir.controllers', ['Sahiplendir.services'])
 	$scope.currentSlide = 0;
 	
 	$scope.posts = [
-		{image:'http://test.clckwrk.im/test/1.jpg', text:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ips'},
+		{image:'http://test.clckwrk.im/test/1.jpg', text:'Tekir kedi, 4 aylık, sevecen'},
 		{image:'http://test.clckwrk.im/test/2.jpg', text:'Sarman, tahmini 4 aylık, aşırı oyuncu'},
 		{image:'http://test.clckwrk.im/test/3.jpg', text:'Labrador, 2 yaşında, İskenderun\'da barınakta'}
 	];
